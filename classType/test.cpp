be854da24e60622c82a424d9bd2cd5e95ff81986
Dylan Warren <duwtgb@mst.edu>
2016-12-11 04:08:48 -0600

    Second header for stats made

--- classType/test.cpp
+++ classType/test.cpp
@@ -16,20 +16,30 @@ int main()
   int x = 10, y = 10;
   initscr();
   start_color();
+  // (red is the char color, black is background)
   init_pair(1,COLOR_RED, COLOR_BLACK);
   noecho();
   //cbreak();
   curs_set(FALSE);
 
   WINDOW *game_win;
-
-  game_win = newwin(90, 90, 0, 0);
-
+  WINDOW *stats_win;
+  //(100 x big, 90 y big, 0 for left top head, 0 head top y)
+  game_win = newwin(100, 90, 0, 0);
+  // same such
+  stats_win = newwin(0, 0, 0, 90);
   while(true)
   {
+    //Giving our guy color.
     wattron(game_win, COLOR_PAIR(1));
+    //Outputting our guy
     mvwprintw(game_win, x,y, a.getMe().c_str());
     wrefresh(game_win);
+
+    //Testing stats header! progress so far.
+    mvwprintw(stats_win, 5, 0, "Me: %s", a.getMe().c_str());
+    wrefresh(stats_win);
+    wclear(stats_win);
     wclear(game_win);
 
     if (getch() == '\033') { // if the first value is esc
