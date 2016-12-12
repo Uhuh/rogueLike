d7fd156410499231fecc07fac197699a328eaaa7
Dylan Warren <duwtgb@mst.edu>
2016-12-12 01:48:54 -0600

    Map stuff

---
+++ classType/map.h
@@ -0,0 +1,29 @@
+#ifndef MAP_H
+#define MAP_H
+#include "player.h"
+#include <cassert>
+#include <vector>
+#include <ncurses.h>
+#include <unistd.h>
+
+
+class map
+{
+  private:
+    std::vector<char> data;
+    int width, height;
+  public:
+    const char& getxy(int x,int y) const { assert(x < width && y < height); return data[x+y*width]; }
+
+    char& getxy(int x,int y) { assert(x < width && y < height); return data[x+y*width]; }
+
+    int getWidth() const { return width; }
+    int getHeight() const { return height; }
+
+    void outMap(WINDOW*, const player&, int, int);
+
+    map(int wd, int ht): width(wd), height(ht), data(wd*ht, '.'){}
+    void build(int x, int y);
+};
+
+#endif
