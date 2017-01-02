598c793c37bb2a419f187ae99e33ec0ec175db64
Dylan Warren <duwtgb@mst.edu>
2017-01-02 15:05:17 -0600

    Making two rooms

--- classType/test.cpp
+++ classType/test.cpp
@@ -5,44 +5,37 @@
 #include <iostream>
 #include <unistd.h>
 #include <cstring>
-#include <cstdlib>
-
-/* Currently working on the vector part of the program.
-   Trying to make the vector the map we can interact with and
-   then we're outputting it with ncurses.
-
-   *Fixed the dot in the right hand corner.
-
-   *Fixed the walls, x and y can now be bigger than one another and build the walls fine.
-
-*/
 
 
 int main()
 {
-  srand(time(NULL));
-
   unit a, b;// a is us, b is monster.
   //going to be using a base class for both players and monster
   //just haven't changed class name.
 
   //constructor => map(width, height)
   //int 1 = row, int 2 = col
-  map hi(40, 60);
-  hi.build(hi.getWidth()-1, hi.getHeight()-1);
+  map world(40, 60);
+  world.build(world.getWidth()-1, world.getHeight()-1);
 
   //Check the rand.cpp for how this is made.
   // Trying out how to make the room by random.
-  hi.buildRoom(hi.getWidth()/rand_int(2, 8), hi.getHeight()/rand_int(2, 8),
-               hi.getWidth()/rand_int(2, 8), hi.getHeight()/rand_int(2, 8));
+  world.buildRoom(world.getWidth()/rand_int(2, 8),
+                  world.getHeight()/rand_int(2, 8),
+                  world.getWidth()/rand_int(2, 8),
+                  world.getHeight()/rand_int(2, 8));
+
+  world.buildRoom(world.getWidth()/rand_int(2, 8),
+                  world.getHeight()/rand_int(2, 8),
+                  world.getWidth()/rand_int(2, 8),
+                  world.getHeight()/rand_int(2, 8));
 
 
-  // Random location for setting character. Make it a funct and check if spawn is on wall.
-  int startx = hi.getWidth()/rand_int(2, 6), starty = hi.getHeight()/rand_int(2, 6);
-  int x = startx, y = starty;
+
+  a.spawn(world);
+  //Using x,y to update ncurses window.
+  int x = a.m_x, y = a.m_y;
   a.setMe('@');
-  a.m_x = startx;
-  a.m_y = starty;
 
   std::string name;
   std::cout << "Speak your name friend and enter: ";
@@ -51,10 +44,7 @@ int main()
 
   b.setName("Goblin");
   b.setMe('G');
-
-  // Setting the monsters location is a work in progress.
-  b.m_x = (hi.getWidth()-(hi.getWidth()/2)/rand_int(2, 8));
-  b.m_y = (hi.getWidth()-(hi.getWidth()/2)/rand_int(2, 8));
+  b.spawn(world);
 
   initscr();
   start_color();
@@ -72,9 +62,9 @@ int main()
   WINDOW *stats_win;
   WINDOW *monster_win;
 
-  game_win = newwin(hi.getWidth(), hi.getHeight(), 0, 0);
-  stats_win = newwin(10, 20, 0, hi.getHeight()+5);
-  monster_win = newwin(10, 20, 30, hi.getHeight()+5);
+  game_win = newwin(world.getWidth(), world.getHeight(), 0, 0);
+  stats_win = newwin(10, 20, 0, world.getHeight()+5);
+  monster_win = newwin(10, 20, 30, world.getHeight()+5);
 
   while(true)
   {
@@ -84,13 +74,13 @@ int main()
     wborder(monster_win, '|', '|', '=','=','*','*','*','*');
 
     //Our guy can see wherever he has gone.""
-    hi.setVis(a.m_x, a.m_y, true);
+    world.setVis(a.m_x, a.m_y, true);
 
     //outputting the map to screen.
-    hi.outMap(game_win);
-    hi.placeMonster(game_win, b);
-    hi.outUser(game_win, a);
-    hi.moveMonster(a, b);
+    world.outMap(game_win);
+    world.placeMonster(game_win, b);
+    world.outUser(game_win, a);
+    world.moveMonster(a, b);
     //outputting the players stats in another window.
     a.outStats(stats_win);
     b.outStats(monster_win);
@@ -102,13 +92,13 @@ int main()
       {
         case 'A':
 
-          if((hi.getxy(a.m_x - 1, a.m_y) == '.'))
+          if((world.getxy(a.m_x - 1, a.m_y) == '.'))
           {
             a.m_x-=1;
-            hi.getxy(a.m_x+1, a.m_y) = '.';
+            world.getxy(a.m_x+1, a.m_y) = '.';
           }
 
-          else if((hi.getxy(a.m_x - 1, a.m_y) == b.getMe()[0]))
+          else if((world.getxy(a.m_x - 1, a.m_y) == b.getMe()[0]))
           {
             b.setHealth(b.getHealth()-1);
             b.outStats(monster_win);
@@ -123,14 +113,14 @@ int main()
 
         case 'B':
 
-          if((hi.getxy( a.m_x + 1, a.m_y) == '.'))
+          if((world.getxy( a.m_x + 1, a.m_y) == '.'))
           {
             a.m_x+=1;
-            hi.getxy(a.m_x-1, a.m_y) = '.';
+            world.getxy(a.m_x-1, a.m_y) = '.';
 
           }
 
-          else if((hi.getxy(a.m_x + 1, a.m_y) == b.getMe()[0]))
+          else if((world.getxy(a.m_x + 1, a.m_y) == b.getMe()[0]))
           {
             b.setHealth(b.getHealth()-1);
             b.outStats(monster_win);
@@ -145,14 +135,14 @@ int main()
 
         case 'C':
 
-          if((hi.getxy( a.m_x , a.m_y + 1) == '.'))
+          if((world.getxy( a.m_x , a.m_y + 1) == '.'))
           {
             a.m_y+=1;
-            hi.getxy(a.m_x, a.m_y-1) = '.';
+            world.getxy(a.m_x, a.m_y-1) = '.';
 
           }
 
-          else if((hi.getxy(a.m_x, a.m_y + 1) == b.getMe()[0]))
+          else if((world.getxy(a.m_x, a.m_y + 1) == b.getMe()[0]))
           {
             b.setHealth(b.getHealth()-1);
             b.outStats(monster_win);
@@ -167,14 +157,14 @@ int main()
 
         case 'D':
 
-          if((hi.getxy( a.m_x, a.m_y - 1) == '.'))
+          if((world.getxy( a.m_x, a.m_y - 1) == '.'))
           {
             a.m_y-=1;
-            hi.getxy(a.m_x, a.m_y+1) = '.';
+            world.getxy(a.m_x, a.m_y+1) = '.';
 
           }
 
-          else if((hi.getxy(a.m_x, a.m_y - 1) == b.getMe()[0]))
+          else if((world.getxy(a.m_x, a.m_y - 1) == b.getMe()[0]))
           {
             b.setHealth(b.getHealth()-1);
             b.outStats(monster_win);
