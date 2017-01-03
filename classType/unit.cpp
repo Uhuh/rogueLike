f3dcb57b0218de459b3181727a8685e5c33ed97e
Dylan Warren <duwtgb@mst.edu>
2017-01-03 09:43:19 -0600

    Changed var names, height to col, width to row.

--- classType/unit.cpp
+++ classType/unit.cpp
@@ -36,8 +36,8 @@ void unit::spawn(const map & area)
 {
   do
   {
-    m_x = (area.getWidth()-(area.getWidth()/2)/rand_int(2, 8));
-    m_y = (area.getWidth()-(area.getWidth()/2)/rand_int(2, 8));
+    m_x = (area.getRow()-(area.getRow()/2)/rand_int(2, 8));
+    m_y = (area.getRow()-(area.getRow()/2)/rand_int(2, 8));
   }while(area.getxy(m_x, m_y) != '.');
   return;
 }
