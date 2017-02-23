e4a1697f3ffb1e379ba6107488d44faee5d662b7
Dylan Warren <duwtgb@mst.edu>
2017-02-22 18:52:37 -0600

    messed with placing user on vector.

--- classType/unit.cpp
+++ classType/unit.cpp
@@ -38,7 +38,7 @@ void unit::spawn(const map & area)
   {
     m_x = (area.getRow()-(area.getRow()/2)/rand_int(2, 8));
     m_y = (area.getRow()-(area.getRow()/2)/rand_int(2, 8));
-  }while(area.getxy(m_x, m_y) != '.');
+  }while(area.getxy(m_x, m_y) == '#');
   return;
 }
 
